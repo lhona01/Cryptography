349bc99678d7c53d2ed2147bd3336a98f3a056f3
Lomus Hona <honalomus9@gmail.com>
2025-03-30 12:03:00 -0500

    birthday attack md5

---
+++ Steganography and Cryptanalysys/Cryptanalysis/Program.cs
@@ -0,0 +1,52 @@
+﻿
+
+using System;
+using System.Security;
+using System.Security.Cryptography;
+
+
+public class Cryptanalysis
+{
+    public static void Main(string[] args)
+    {
+        string secretString = args[0];
+        string cipherString = args[1];
+        bool seedFound = false;
+        
+        DateTime startDt = new DateTime(2020, 7, 3, 11, 0, 0);
+        DateTime endDt = new DateTime(2020, 7, 4, 11, 0, 0);
+
+        for (DateTime currDt = startDt; startDt < endDt; currDt = currDt.AddMinutes(1))
+        {
+            TimeSpan ts = currDt.Subtract(new DateTime(1970, 1, 1));
+            
+            Random rng = new Random((int)ts.TotalMinutes);
+            byte[] key = BitConverter.GetBytes(rng.NextDouble());
+
+            if (cipherString == Encrypt(key, secretString))
+            {
+                Console.WriteLine(ts.TotalMinutes);
+                seedFound = true;
+                break;
+            }
+        }
+
+        if (seedFound == false)
+        {
+            Console.WriteLine("seed not found!");
+        }
+    }
+    private static string Encrypt(byte[] key, string secretString)
+    {
+        DESCryptoServiceProvider csp = new DESCryptoServiceProvider();
+        MemoryStream ms = new MemoryStream();
+        CryptoStream cs = new CryptoStream(ms,
+        csp.CreateEncryptor(key, key), CryptoStreamMode.Write);
+        StreamWriter sw = new StreamWriter(cs);
+        sw.Write(secretString);
+        sw.Flush();
+        cs.FlushFinalBlock();
+        sw.Flush();
+        return Convert.ToBase64String(ms.GetBuffer(), 0, (int)ms.Length);
+    }
+}
---
+++ hash_md5_birthday_attack/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+class Hash
+{
+    private static readonly Random _random = new Random();
+
+    public static void Main(string[] args)
+    {
+        Dictionary<string, string> md5Hash = new Dictionary<string, string>();
+        bool collision = false;
+
+        if (args.Length != 1)
+        {
+            Console.WriteLine("Usage: dotnet run <salt_in_hex>");
+            return;
+        }
+
+        string saltHex = args[0];
+        byte saltByte = Convert.ToByte(saltHex, 16);
+
+        while (!collision)
+        {
+            string randomString = GetRandomString(10);
+            byte[] stringBytes = Encoding.UTF8.GetBytes(randomString);
+            byte[] bytesToHash = new byte[stringBytes.Length + 1];
+            Array.Copy(stringBytes, bytesToHash, stringBytes.Length);
+            bytesToHash[bytesToHash.Length - 1] = saltByte;
+
+            byte[] hashBytes = MD5.Create().ComputeHash(bytesToHash);
+
+            byte[] first5Bytes = new byte[5];
+            Array.Copy(hashBytes, 0, first5Bytes, 0, 5);
+
+            string hashString = BitConverter.ToString(first5Bytes).Replace("-", "");
+            if (md5Hash.ContainsKey(hashString))
+            {
+                if (md5Hash[hashString] != randomString)
+                {
+                    Console.WriteLine($"{md5Hash[hashString]},{randomString}");
+                    collision = true;
+                    break;
+                }
+            }
+            else
+            {
+                md5Hash.Add(hashString, randomString);
+            }
+        }
+    }
+
+    public static string GetRandomString(int length)
+    {
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++)
+        {
+            sb.Append(GetRandomCharacter());
+        }
+        return sb.ToString();
+    }
+
+    public static char GetRandomCharacter()
+    {
+        int choice = _random.Next(3);
+
+        if (choice == 0) // Digits 0-9
+        {
+            return (char)_random.Next(48, 58); // ASCII 48-57
+        }
+        else if (choice == 1) // Lowercase a-z
+        {
+            return (char)_random.Next(97, 123); // ASCII 97-122
+        }
+        else // Uppercase A-Z
+        {
+            return (char)_random.Next(65, 91); // ASCII 65-90
+        }
+    }
+}
